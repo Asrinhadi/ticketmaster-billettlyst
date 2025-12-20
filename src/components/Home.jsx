07a904ba4b9d3ceeafdadf9cf39e6d8c4f2c61e6
Asrin Hadi <asrinh@hiof.no>
2025-12-20 19:03:44 +0100

    Home: fetceher 10 eventer i en by og rendrer en liste av dett

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getTenEventsByCity } from "../services/ticketmasterServices";
 
 export default function Home() {
   const cities = ["Oslo", "Berlin", "London"];
-  const [selectedCity, setSelectedCity] = useState(cities[0]);
+  const [selectedCity, setSelectedCity] = useState("Oslo");
+  const [cityEvents, setCityEvents] = useState([]);
+
+  const loadCityEvents = async (cityName) => {
+    const events = await getTenEventsByCity(cityName);
+    setCityEvents(events);
+    setSelectedCity(cityName);
+  };
+
+  useEffect(() => {
+    loadCityEvents("Oslo");
+  }, []);
 
   return (
     <main>
@@ -10,15 +22,17 @@ export default function Home() {
 
       <section>
         {cities.map((city) => (
-          <button key={city} onClick={() => setSelectedCity(city)}>
+          <button key={city} onClick={() => loadCityEvents(city)}>
             {city}
           </button>
         ))}
       </section>
 
-      <section>
-        <p>Kommer snart…</p>
-      </section>
+      <ul>
+        {cityEvents.map((event) => (
+          <li key={event.id}>{event.name}</li>
+        ))}
+      </ul>
     </main>
   );
 }
