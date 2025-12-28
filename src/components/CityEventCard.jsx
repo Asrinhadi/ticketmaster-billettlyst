8823915ddc764115de16141aa3a150a873a2fe77
Asrin Hadi <asrinh@hiof.no>
2025-12-28 18:31:26 +0100

    gjort cityventcard ikke klikkbare og oppdaterte dens SCSS

--- src/components/CityEventCard.jsx
+++ src/components/CityEventCard.jsx
@@ -1,30 +1,27 @@
-import { Link } from "react-router-dom";
 import { formatDate, getImage } from "../assets/utils/helpers";
 import "../styles/CityEventCard.scss";
 
 export default function CityEventCard({ event }) {
   const venue = event?._embedded?.venues?.[0];
-  const attractionId = event?._embedded?.attractions?.[0]?.id;
 
   return (
     <li className="city-card">
-      <img src={getImage(event)} alt={event?.name || "Event"} />
-      
+      <img
+        src={getImage(event)}
+        alt={event?.name || "Event"}
+      />
+
       <section className="card-info">
         <h3>{event?.name || "Uten navn"}</h3>
         <p>{venue?.country?.name || "Ukjent land"}</p>
         <p>{venue?.city?.name || "Ukjent by"}</p>
-        <p className="date">{formatDate(event?.dates?.start?.localDate)}</p>
-        
-        {attractionId && (
-          <Link to={`/event/${attractionId}`} className="details-link">Les mer</Link>
-        )}
+        <p className="date">
+          {formatDate(event?.dates?.start?.localDate)}
+        </p>
       </section>
     </li>
   );
 }
 
-
-
-//(_embedded.venues[0]) fra Ticketmaster API
+// (_embedded.venues[0]) fra Ticketmaster API
 // Ref: https://developer.ticketmaster.com/products-and-docs/tutorials/events-search/search_events_with_discovery_api.html
