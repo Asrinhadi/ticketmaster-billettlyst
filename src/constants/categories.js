b65c079dd0e0784be2f080e78dc3d188d7f4b4c2
Asrin Hadi <asrinh@hiof.no>
2025-12-28 21:27:20 +0100

    Oppdaterer kategori-konstanter med segmentId og slug

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,6 +1,8 @@
-// kategorier fra ticketmaster api
+
 export const CATEGORIES = [
-  { id: "KZFzniwnSyZfZ7v7nJ", name: "Musikk", slug: "musikk" },
-  { id: "KZFzniwnSyZfZ7v7nE", name: "Sport", slug: "sport" },
-  { id: "KZFzniwnSyZfZ7v7na", name: "Teater", slug: "teater" }
+  { id: "KZFzniwnSyZfZ7v7nJ", name: "Musikk", slug: "music" },
+  { id: "KZFzniwnSyZfZ7v7nE", name: "Sport", slug: "sports" },
+  { id: "KZFzniwnSyZfZ7v7na", name: "Teater", slug: "arts" }
 ];
+
+// kategorier fra ticketmaster api
